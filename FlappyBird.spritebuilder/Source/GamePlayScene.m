7b8eeb6152e15a9dd2a4868419fcb5ff38eaa646
Joshua Nordstrom <nordstromenator@gmail.com>
2014-06-25 08:00:00 -0500

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -7,6 +7,10 @@
 - (void)initialize
 {
     // your code here
+    character = (Character*)[CCBReader load:@"Character"];
+    [physicsNode addChild:character];
+    
+    [self addObstacle];
 }
 
 -(void)update:(CCTime)delta
@@ -15,5 +19,10 @@
 }
 
 // put new methods here
+-(void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event
+{
+    // this will get valled every time the screen is touched
+    [character flap];
+}
 
 @end
